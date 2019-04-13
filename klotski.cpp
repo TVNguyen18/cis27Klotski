84f87ff3574b6fe63c2becd10c3b01535fef635d
TVNguyen18 <49044385+TVNguyen18@users.noreply.github.com>
2019-04-12 22:13:29 -0700

    The project file

--- klotski.cpp
+++ klotski.cpp
@@ -4,7 +4,30 @@
 using namespace std;
 
 int main() {
+	GLFWwindow* window;
 
+	if (!glfwInit())
+		return -1;
 
+	window = glfwCreateWindow(400, 480, "Hello World", NULL, NULL);
 
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+		cout << "\nERROR!" << endl;
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
